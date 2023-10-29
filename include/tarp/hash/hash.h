12ffc9393ee14344f122992239c9f962647bd25a
vcsaturninus <vcsaturninus@protonmail.com>
2023-10-29 18:32:54 +0000

    chore: add some basic hash functions for hash table use

---
+++ include/tarp/hash/hash.h
@@ -0,0 +1,26 @@
+#ifndef TARP_HASH_H
+#define TARP_HASH_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include <stdint.h>
+#include <stdlib.h>
+
+
+/*
+ * Hash functions for use with hash tables
+ */
+
+uint64_t jenkins_oaat(const uint8_t *key, size_t len);
+uint64_t djb2(const uint8_t key[], size_t len);
+
+
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif
---
+++ src/misc/hash.c
@@ -0,0 +1,41 @@
+#include <endian.h>
+#include <tarp/common.h>
+#include <tarp/hash/md5sum.h>
+#include <tarp/hash/hash.h>
+
+#include <string.h>
+#include <stdint.h>
+
+/*
+ * Jenkins, one at a time */
+uint64_t jenkins_oaat(const uint8_t key[], size_t len){
+    assert(key); assert(len > 0);
+    size_t i = 0;
+    uint64_t hash = 0;
+
+    while (i < len) {
+        hash += key[i++];
+        hash += hash << 10;
+        hash ^= hash >> 6;
+    }
+
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash;
+}
+
+uint64_t djb2(const uint8_t key[], size_t len){
+    uint64_t hash = 5381;
+    size_t i = 0;
+    int c;
+
+    while (i < len){
+        c = key[i];
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        ++i;
+    }
+
+    return hash;
+}
+
