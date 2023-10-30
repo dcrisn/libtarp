216bac31ee6daf88a21ffb0b7b182dad20099f71
vcsaturninus <vcsaturninus@protonmail.com>
2023-10-30 11:42:20 +0000

    chore: clean up and improve float helpers

--- include/tarp/floats.h
+++ include/tarp/floats.h
@@ -1,31 +1,53 @@
-#ifndef MO_FLOAT_H
-#define MO_FLOAT_H
+#ifndef TARP_FLOATS_H
+#define TARP_FLOATS_H
+
+#include <math.h>
 
 /*
- * Includes:
- *  - math.h
- *
- *
- *  Must link with:
- *  - lm
+ * Some helpers for working with floating point numbers.
+ * Do be very aware of the following issues:
  */
 
-/* 
- * Return the integral part of n and discard the decimal part */
-long get_integral_part(double n);
+/*
+ * Perform a conversion from double to long - if safe.
+ *
+ * --> d
+ * The double to convert. Must not be NaN or infinity.
+ *
+ * --> fallback
+ * Value to return if the conversion is unsafe.
+ *
+ * --> truncate
+ * If true, d is truncated (rounded *down*) to the nearest integer.
+ * Otherwise if false, d is rounded to the nearest integer
+ * (see man 3 round).
+ *
+ * --> safe
+ * if not NULL, boolean true will be stored here if the conversion
+ * was safe and successful, otherwise false if unsafe and unsuccessful.
+ *
+ * <-- return
+ * The conversion result as a long if safe; otherwise fallback.
+ */
+long db2long(double d, bool truncate, long fallback, bool *safe);
 
 /*
- * Return the decimal part of n and discard the integral part */
-double get_decimal_part(double n);
+ * Return the integral/decimal part of d */
+double get_integral_part(double d);
+double get_decimal_part(double d);
 
 /*
- * Discard all digits > DP decimal places without any rounding.
- * */
-double truncateto(double n, int dp);
+ * Keep only n decimal places. Truncate the rest.
+ * Note the last digit before cutoff point is not rounded.
+ * E.g. in: d=8.2318, out: truncate(d, 3)=8.231
+ *
+ * If d has fewer decimal places than n, then nothing is done.
+ */
+double truncateto(double d, int numdp);
 
 /*
  * Round n to PRECISION decimal places and return the result */
-double roundto(double n, int precision);
+double roundto(double d, int precision);
 
 /*
  * Compare two floating point number; the result returned
--- src/misc/floats.c
+++ src/misc/floats.c
@@ -1,34 +1,77 @@
+#include <limits.h>
 #include <math.h>  /* pow(), round() */
 #include <float.h> /* FLT_EPSILON) */
 
+#include <tarp/common.h>
+#include <tarp/bits.h>
 #include <tarp/floats.h>
 
 /*
- * Truncate off the decimal part -- equivalent to trunc() in math.h .*/
-long get_integral_part(double n){
-    return (long) n;
+ * Ensure casting from a long to a double is always safe (even though
+ * precision may be lost) */
+static_assert(sizeof(long) <= sizeof(double), "sizeof(long)>sizeof(double)");
+
+long db2long(double d, bool truncate, long fallback, bool *safe){
+    assert(!isnan(d));
+    assert(!isinf(d));
+
+    d = truncate ? trunc(d) : round(d);
+
+    /*
+     * maximum value of long as a double */
+    double limit = ldexp(1.0, bytes2bits(sizeof(long))-1);
+
+    if (d >= -(limit) && d < limit){
+        *safe = true;
+        /* safe because the value being cast is within the range of
+         * a signed long [-2^63, +2^63) */
+        return (long)d;
+    }
+
+    *safe = false;
+    return fallback;
 }
 
-double get_decimal_part(double n){
-    return n - get_integral_part(n);
+
+/*
+ * Truncate off the decimal part -- equivalent to trunc() in math.h.
+ *
+ * NOTE mofd may too slow if performance is critical, since it deals
+ * with various cases. If you are sure the value is within bounds and
+ * does not need to be range-checked, then just cast to an appropriate
+ * integral type directly.
+ * */
+double get_integral_part(double d){
+    double integral = 0.0;
+    modf(d, &integral);
+    return integral;
 }
 
-double truncateto(double n, int dp){
-    long scaling_factor = pow(10, dp);
-    long truncated = (long) (n * scaling_factor);
-    return 
-        ((double) truncated) / scaling_factor;
+double get_decimal_part(double d){
+    double integral;
+    return modf(d, &integral);
 }
 
-double roundto(double n, int precision){
-    long scaling_factor = pow(10, precision);
-    return round(n * scaling_factor) / scaling_factor;
+double truncateto(double d, int numdp){
+    double scaling_factor = pow(10, numdp);
+    double integral, decimal;
+    decimal = modf(d, &integral);
+
+    return integral + (trunc(decimal * scaling_factor) / scaling_factor);
+}
+
+double roundto(double d, int numdp){
+    double scaling_factor = pow(10, numdp);
+    double integral, decimal;
+    decimal = modf(d, &integral);
+
+    return integral + (round(decimal * scaling_factor) / scaling_factor);
 }
 
 int dbcmp(double a, double b, double epsilon){
-       if(fabs(a - b) < (epsilon > 0 ? epsilon : FLT_EPSILON) ){
-          return 0;
+       if(fabs(a - b) < ((epsilon > 0) ? epsilon : FLT_EPSILON) ){
+          return EQ;
     }
-    return a > b ? 1 : -1;
+    return a > b ? GT : LT;
 }
 
