b2c7c9da15570b7082537121254666fc62b6f660
vcsaturninus <vcsaturninus@protonmail.com>
2024-03-23 09:56:18 +0000

    feat: implement a convenience Timer class

---
+++ include/tarp/timer.hxx
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <chrono>
+
+namespace tarp {
+
+/*
+ * Convenience class to get the elapsed duration between two points
+ * in time to a specified resolution (milliseconds by default).
+ */
+template<typename resolution = std::chrono::milliseconds>
+class Timer {
+public:
+    Timer() : m_start_time(), m_stop_time() {};
+
+    void start(void) { m_start_time = clock_type::now(); }
+
+    void stop(void) { m_stop_time = clock_type::now(); }
+
+    resolution get_time(void) const {
+        auto time = m_stop_time - m_start_time;
+        return std::chrono::duration_cast<resolution>(time);
+    }
+
+private:
+    using clock_type = std::chrono::high_resolution_clock;
+    std::chrono::time_point<clock_type> m_start_time;
+    std::chrono::time_point<clock_type> m_stop_time;
+};
+
+}  // namespace tarp
