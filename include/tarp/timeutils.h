24df891d2497fcc07481f76dcaefbbd03fdc20b4
vcsaturninus <vcsaturninus@protonmail.com>
2024-06-14 18:15:50 +0100

    chore(timeutils): add convenience function for getting a unix timestamp in the future
    + run clang-format on the timeutils.{h,c}xx files

--- include/tarp/timeutils.h
+++ include/tarp/timeutils.h
@@ -12,11 +12,11 @@ extern "C" {
 #include <tarp/common.h>
 
 
-#define MSECS_PER_SEC  1000U         /* 1e3 */
-#define USECS_PER_SEC  1000000U      /* 1e6 */
-#define NSECS_PER_SEC  1000000000LU  /* 1e9 */
-#define NSECS_PER_MSEC 1000000U      /* 1e6 */
-#define NSECS_PER_USEC 1000U         /* 1e3 */
+#define MSECS_PER_SEC  1000U        /* 1e3 */
+#define USECS_PER_SEC  1000000U     /* 1e6 */
+#define NSECS_PER_SEC  1000000000LU /* 1e9 */
+#define NSECS_PER_MSEC 1000000U     /* 1e6 */
+#define NSECS_PER_USEC 1000U        /* 1e3 */
 
 /*
  * timespec to double as the number of seconds or milliseconds */
@@ -32,20 +32,24 @@ struct timespec time_now_monotonic(void);
 /* get the NOW timepoint on the CLOCK_REALTIME system clock */
 struct timespec time_now_epoch(void);
 
+/* Get the time since the unix epoch + ms. This can be used for
+ * computing an absolute time in the future as a unix timestamp. */
+struct timespec get_unix_timepoint(unsigned ms);
+
 /* like time_now_monotonic, but convert to double;
  * see timespec2db{s,ms} fmi. */
 double time_now_monotonic_dbms(void);
 double time_now_monotonic_dbs(void);
 
 /* compare 2 timespecs; look for 'comparator' in common.h fmi */
-enum comparatorResult timespec_cmp(const struct timespec *a, const struct timespec *b);
+enum comparatorResult timespec_cmp(const struct timespec *a,
+                                   const struct timespec *b);
 
 /* add 2 timespecs; note if unreasonable values are fed in as input, the
  * timespecs (the seconds member) may possibly overflow. */
-void timespec_add(
-        const struct timespec *a,
-        const struct timespec *b,
-        struct timespec *c);
+void timespec_add(const struct timespec *a,
+                  const struct timespec *b,
+                  struct timespec *c);
 
 /*
  * Convert ms to a timespec and return it. */
@@ -65,7 +69,7 @@ int mssleep(uint32_t ms, bool uninterruptible);
 
 
 #ifdef __cplusplus
-}  /* extern "C" */
+} /* extern "C" */
 #endif
 
 
@@ -78,17 +82,15 @@ int mssleep(uint32_t ms, bool uninterruptible);
 /*
  * Convert a chrono duration to a timespec and throw an exception
  * if the tv_sec member of the timespec overflows */
-template <typename chrono_duration>
-void chrono2timespec(
-        chrono_duration duration, struct timespec *tspec,
-        bool throw_on_overflow=false);
+template<typename chrono_duration>
+void chrono2timespec(chrono_duration duration,
+                     struct timespec *tspec,
+                     bool throw_on_overflow = false);
 
-template <>
-void chrono2timespec<std::chrono::seconds>(
-        std::chrono::seconds secs,
-        struct timespec *tspec,
-        bool throw_on_overflow)
-{
+template<>
+void chrono2timespec<std::chrono::seconds>(std::chrono::seconds secs,
+                                           struct timespec *tspec,
+                                           bool throw_on_overflow) {
     assert(tspec);
     tspec->tv_nsec = 0;
     tspec->tv_sec = secs.count();
@@ -96,45 +98,41 @@ void chrono2timespec<std::chrono::seconds>(
     // tv_sec is bigger than chrono's seconds duration so this will never
     // overflow but is kept for consistency. Also, if the c++ standard
     // changes the number of bits, it is a good sanity check.
-    if (throw_on_overflow){
+    if (throw_on_overflow) {
         if (secs.count() != tspec->tv_sec)
             throw std::overflow_error("timespec overflow");
     }
 }
 
-template <>
-void chrono2timespec<std::chrono::milliseconds>(
-        std::chrono::milliseconds ms,
-        struct timespec *tspec,
-        bool throw_on_overflow)
-{
+template<>
+void chrono2timespec<std::chrono::milliseconds>(std::chrono::milliseconds ms,
+                                                struct timespec *tspec,
+                                                bool throw_on_overflow) {
     assert(tspec);
     tspec->tv_nsec = (ms.count() % MSECS_PER_SEC) * NSECS_PER_MSEC;
     tspec->tv_sec = ms.count() / MSECS_PER_SEC;
 
-    if (throw_on_overflow){
+    if (throw_on_overflow) {
         if (ms.count() / MSECS_PER_SEC != tspec->tv_sec)
             throw std::overflow_error("timespec overflow");
     }
 }
 
-template <>
-void chrono2timespec<std::chrono::microseconds>(
-        std::chrono::microseconds us,
-        struct timespec *tspec,
-        bool throw_on_overflow)
-{
+template<>
+void chrono2timespec<std::chrono::microseconds>(std::chrono::microseconds us,
+                                                struct timespec *tspec,
+                                                bool throw_on_overflow) {
     assert(tspec);
     tspec->tv_nsec = (us.count() % USECS_PER_SEC) * NSECS_PER_USEC;
     tspec->tv_sec = us.count() / USECS_PER_SEC;
 
-    if (throw_on_overflow){
+    if (throw_on_overflow) {
         if (us.count() / USECS_PER_SEC != tspec->tv_sec)
             throw std::overflow_error("timespec overflow");
     }
 }
 
-#endif   /* __cplusplus */
+#endif /* __cplusplus */
 
 
-#endif  /* TARP_TIMEUTILS_H  */
+#endif /* TARP_TIMEUTILS_H  */
--- src/misc/timeutils.c
+++ src/misc/timeutils.c
@@ -1,11 +1,11 @@
-#include <time.h>
 #include <assert.h>
 #include <errno.h>
+#include <time.h>
 
 #include <tarp/floats.h>
-#include <tarp/timeutils.h>
-#include <tarp/math.h>
 #include <tarp/log.h>
+#include <tarp/math.h>
+#include <tarp/timeutils.h>
 
 // see man (2) clock_gettime
 #define TV_NSEC_MAX 999999999LU
@@ -14,26 +14,25 @@
  * Convert ts to a double in the following format:
  * <seconds * secs_multiplier>.<nanoseconds / nsecs_divisor>
  */
-static double timespec2db(
-        const struct timespec *ts,
-        unsigned secs_multiplier,
-        unsigned nsecs_divisor)
-{
+static double timespec2db(const struct timespec *ts,
+                          unsigned secs_multiplier,
+                          unsigned nsecs_divisor) {
     assert(ts);
-    return (double)ts->tv_sec * secs_multiplier + (double)ts->tv_nsec / nsecs_divisor;
+    return (double)ts->tv_sec * secs_multiplier +
+           (double)ts->tv_nsec / nsecs_divisor;
 }
 
-double timespec2dbs(const struct timespec *time){
+double timespec2dbs(const struct timespec *time) {
     assert(time);
     return timespec2db(time, 1, NSECS_PER_SEC);
 }
 
-double timespec2dbms(const struct timespec *time){
+double timespec2dbms(const struct timespec *time) {
     assert(time);
     return timespec2db(time, MSECS_PER_SEC, NSECS_PER_MSEC);
 }
 
-double timespec2dbus(const struct timespec *time){
+double timespec2dbus(const struct timespec *time) {
     assert(time);
     return timespec2db(time, USECS_PER_SEC, NSECS_PER_USEC);
 }
@@ -41,30 +40,45 @@ double timespec2dbus(const struct timespec *time){
 /*
  * These functions should not fail given the way they are called;
  * there's no room for the arguments to be invalid etc. */
-struct timespec time_now_monotonic(void){
+struct timespec time_now_monotonic(void) {
     struct timespec ts;
     memset(&ts, 0, sizeof(struct timespec));
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return ts;
 }
 
-struct timespec time_now_epoch(void){
+struct timespec time_now_epoch(void) {
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
     return ts;
 }
 
-double time_now_monotonic_dbs(void){
+struct timespec get_unix_timepoint(unsigned ms) {
+    struct timespec now = time_now_epoch();
+
+    struct timespec delay;
+    delay.tv_sec = ms / MSECS_PER_SEC;
+
+    // leftover milliseconds (< 1s)
+    ms %= MSECS_PER_SEC;
+    delay.tv_nsec = ms * NSECS_PER_MSEC;
+
+    struct timespec ts;
+    timespec_add(&now, &delay, &ts);
+    return ts;
+}
+
+double time_now_monotonic_dbs(void) {
     struct timespec ts = time_now_monotonic();
     return timespec2dbs(&ts);
 }
 
-double time_now_monotonic_dbms(void){
+double time_now_monotonic_dbms(void) {
     struct timespec ts = time_now_monotonic();
     return timespec2dbms(&ts);
 }
 
-int db2timespec(double secs, struct timespec *ts){
+int db2timespec(double secs, struct timespec *ts) {
     assert(secs >= 0);
     assert(ts);
 
@@ -81,20 +95,21 @@ int db2timespec(double secs, struct timespec *ts){
     return 0;
 }
 
-enum comparatorResult timespec_cmp(const struct timespec *a, const struct timespec *b){
+enum comparatorResult timespec_cmp(const struct timespec *a,
+                                   const struct timespec *b) {
     assert(a);
     assert(b);
 
-    if (a->tv_sec > b->tv_sec){
+    if (a->tv_sec > b->tv_sec) {
         return GT;
-    } else if (a->tv_sec < b->tv_sec){
+    } else if (a->tv_sec < b->tv_sec) {
         return LT;
     }
 
     /* else, use nanosecond discriminant */
-    if (a->tv_nsec > b->tv_nsec){
+    if (a->tv_nsec > b->tv_nsec) {
         return GT;
-    } else if (a->tv_nsec < b->tv_nsec){
+    } else if (a->tv_nsec < b->tv_nsec) {
         return LT;
     }
 
@@ -108,17 +123,17 @@ enum comparatorResult timespec_cmp(const struct timespec *a, const struct timesp
  * user should be careful of the inputs.
  *
  * NOTE: time_t is normally signed; this library is compiled with -frapv
- * to ensure defined signed wraparound.
+ * to ensure well-defined wraparound for signed integers.
  */
-void timespec_add(
-        const struct timespec *a,
-        const struct timespec *b,
-        struct timespec *c)
-{
-    assert(a); assert(b); assert(c);
+void timespec_add(const struct timespec *a,
+                  const struct timespec *b,
+                  struct timespec *c) {
+    assert(a);
+    assert(b);
+    assert(c);
 
-    time_t secs = a->tv_sec + b->tv_sec; // may wrap around
-    uint64_t nsecs =  a->tv_nsec + b->tv_nsec;
+    time_t secs = a->tv_sec + b->tv_sec;  // may wrap around
+    uint64_t nsecs = a->tv_nsec + b->tv_nsec;
 
     secs += nsecs / NSECS_PER_SEC; /* / 10^9 */
     nsecs = nsecs % NSECS_PER_SEC;
@@ -126,14 +141,14 @@ void timespec_add(
     c->tv_nsec = nsecs;
 }
 
-struct timespec ms2timespec(uint32_t ms){
+struct timespec ms2timespec(uint32_t ms) {
     struct timespec ts = {0};
     ts.tv_sec = ms / MSECS_PER_SEC;
     ts.tv_nsec = (ms % MSECS_PER_SEC) * NSECS_PER_MSEC;
     return ts;
 }
 
-int mssleep(uint32_t ms, bool uninterruptible){
+int mssleep(uint32_t ms, bool uninterruptible) {
     struct timespec ts = time_now_monotonic();
     struct timespec millis = ms2timespec(ms);
     timespec_add(&ts, &millis, &ts);
@@ -141,14 +156,15 @@ int mssleep(uint32_t ms, bool uninterruptible){
     int rc = 0;
 
     // continue if interrupted
-    while ((rc=(clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, NULL) == -1))
-            && uninterruptible && errno==EINTR){
+    while ((rc = (clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, NULL) ==
+                  -1)) &&
+           uninterruptible && errno == EINTR) {
         errno = 0;
     }
 
-    if (rc){  /* an error other than EINTR */
-        error("clock_nanosleep() error in %s() : '%s'",
-                __func__, strerror(errno));
+    if (rc) { /* an error other than EINTR */
+        error(
+          "clock_nanosleep() error in %s() : '%s'", __func__, strerror(errno));
         return -1;
     }
 
