e4a6df2887a2150346b0c5f0a37a9ddb3e27eb21
vcsaturninus <vcsaturninus@protonmail.com>
2024-06-28 23:54:23 +0100

    chore (watchdog): update watchdog header

--- include/tarp/watchdog.hxx
+++ include/tarp/watchdog.hxx
@@ -3,8 +3,10 @@
 
 #include <chrono>
 #include <functional>
+#include <mutex>
+#include <stdexcept>
 
-#include <tarp/thread_entity.hxx>
+#include <tarp/threading.hxx>
 
 namespace tarp {
 
@@ -31,12 +33,12 @@ using wd_action_cb_t = std::function<void(void)>;
  * the watchdog from triggering.
  *
  * NOTE:
- * A paused watchdog can be resumed via another call to run().
+ * A paused watchdog can be resumed via a call to kick().
  * When this happens, the watchdog implicitly resets itself as well
  * so that it is not instantlly triggered again.
  */
 template<typename T = std::chrono::seconds>
-class Watchdog : public tarp::ThreadEntity {
+class Watchdog : public tarp::threading::ThreadEntity {
 public:
     /* guarded-mode watchdog constructor */
     Watchdog(T interval, wd_action_cb_t action, wd_guard_cb_t guard)
@@ -53,34 +55,53 @@ public:
         , m_guard() {}
 
     /* Reset the watchdog so that it waits another full interval
-     * before it 'bites'. I.e. keep the watchdog at bay. */
-    bool reset(void);
+     * before it 'bites'. I.e. keep the watchdog at bay.
+     * NOTE: a watchdog that has already 'bitten' must be 'kicked'; .reset()
+     * is only for a running watchdog.
+     * NOTE: a watchdog that has been stopped can never used again. Reset
+     * has no effect on a stopped watchdog. */
+    void reset();
 
     /* Called either manually or automatically on failure to
      * reset the watchdog.
      * This in turn calls the user-supplied action callback.
      * The watchdog will be paused after this and can be resumed
-     * (assuming the process is not exited) with run().
+     * (assuming the process is not exited) with kick().
      * When that is done, the watchdog is implicitly reset as well
      * so that it doesn't instantly trigger again. */
     void bite(void);
 
+    /*
+     * Re-arm the watchdog if paused. NOP if the watchdog is not paused.
+     * NOTE: paused is *not* the same as stopped. A stopped watchdog cannot
+     * restarted. */
+    void kick();
+
 private:
     void initialize(void) override;
-    void prepare_resume(void) override;
-    void do_task(void) override;
+    void do_work(void) override;
 
     bool m_guard_mode;
     T m_interval;
     std::chrono::steady_clock::time_point m_deadline;
     wd_action_cb_t m_action_cb;
     wd_guard_cb_t m_guard;
+    mutable std::mutex m_mtx;
 };
 
 template<typename T>
-bool Watchdog<T>::reset() {
+void Watchdog<T>::reset() {
     // printf("****** WATCHDOG RESET\n");
-    initialize();
+
+    {
+        LOCK(m_mtx);
+        if (!is_running()) {
+            return;
+        }
+        initialize();
+    }
+
+    signal();
 }
 
 template<typename T>
@@ -89,35 +110,56 @@ void Watchdog<T>::initialize() {
 }
 
 template<typename T>
-void Watchdog<T>::prepare_resume() {
-    initialize();
-}
-
-template<typename T>
 void Watchdog<T>::bite() {
+    LOCK(m_mtx);
+
     // printf("****** WATCHDOG TRIGGERED\n");
+    if (!m_action_cb) {
+        throw std::logic_error("Illegal null action callback");
+    }
     m_action_cb();
-    set_state(PAUSED);
+    set_state(threadState::PAUSED);
 }
 
 template<typename T>
-void Watchdog<T>::do_task(void) {
-    /* NOTE: this may be updated by the user in a separate thread
-     * by calling reset(). If we miss it, we'll get it in the next
-     * pass, so no harm done, and no locking necessary. */
-    if (std::chrono::steady_clock::now() < m_deadline) {
-        std::this_thread::sleep_for(m_interval);
+void Watchdog<T>::do_work(void) {
+    decltype(m_deadline) deadline;
+    {
+        LOCK(m_mtx);
+        deadline = m_deadline;
+    }
+
+    if (std::chrono::steady_clock::now() < deadline) {
+        wait_for(m_interval);
         return;
     }
 
-    if (m_guard_mode) {
-        if (m_guard()) {
-            return;
+    {
+        LOCK(m_mtx);
+        if (m_guard_mode) {
+            if (m_guard()) {
+                return;
+            }
         }
     }
 
     bite();
 }
+
+template<typename T>
+void Watchdog<T>::kick() {
+    if (is_running()) {
+        return;
+    }
+
+    {
+        std::unique_lock l {m_mtx};
+        initialize();
+    }
+
+    run();
+}
+
 
 }  // namespace tarp
 
