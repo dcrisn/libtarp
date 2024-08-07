596d7bee6dd42d85f5430955e9fefefd7df42e20
vcsaturninus <vcsaturninus@protonmail.com>
2024-08-07 19:04:51 +0100

    chore: add semaphore class -- useful pre-C++20.
    + NOTE: likely to be useful post-C++20 as well. Could be updated to use
      various mechanisms such as eventfd so it can be epolled easily.

---
+++ include/tarp/semaphore.hxx
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+
+#include <tarp/cxxcommon.hxx>
+
+namespace tarp {
+
+/* Semaphore implementation -- only useful pre c++20, when semaphores are
+ * introduced in the c++ stdlib.
+ * NOTE: set max_count=2 if you need binary semaphore semantics (or better, use
+ * the binary_semaphore alias).
+ */
+template<std::uint32_t max_count = std::numeric_limits<std::uint32_t>::max()>
+class semaphore {
+public:
+    DISALLOW_COPY_AND_MOVE(semaphore<max_count>);
+
+    /* counter initialized to 0 (i.e. semaphore=locked) by default. */
+    semaphore() : m_counter(0), m_max_count(max_count) {}
+
+public:
+    // reset semaphore to 0
+    void reset() {
+        std::unique_lock l {m_mtx};
+        m_counter = 0;
+    }
+
+    void release() {
+        std::unique_lock l {m_mtx};
+
+        /* this has binary semaphore semantics when m_max_count=2 */
+        if (m_counter < m_max_count) {
+            m_counter++;
+        }
+        m_condvar.notify_all();
+    }
+
+    void acquire() {
+        std::unique_lock l(m_mtx);
+
+        while (m_counter == 0) {
+            m_condvar.wait(l);
+        }
+        --m_counter;
+    }
+
+    bool try_acquire() {
+        std::unique_lock l {m_mtx};
+
+        if (m_counter > 0) {
+            --m_counter;
+            return true;
+        }
+        return false;
+    }
+
+    template<typename timepoint>
+    bool try_acquire_until(const timepoint &abs_time) {
+        std::unique_lock l(m_mtx);
+
+        m_condvar.wait_until(l, abs_time, [this] {
+            return m_counter > 0;
+        });
+
+        if (m_counter > 0) {
+            --m_counter;
+            return true;
+        }
+        return false;
+    }
+
+    template<class Rep, class Period>
+    bool try_acquire_for(const std::chrono::duration<Rep, Period> &rel_time) {
+        return try_acquire_until(std::chrono::steady_clock::now() + rel_time);
+    }
+
+private:
+    std::mutex m_mtx;
+    std::condition_variable m_condvar;
+    uint32_t m_counter;
+    const std::uint32_t m_max_count;
+};
+
+using binary_semaphore = semaphore<2>;
+
+}  // namespace tarp
