c3bca94fb8449934e800d753593f016ace3f31f7
vcsaturninus <vcsaturninus@protonmail.com>
2025-01-31 18:20:13 +0000

    chore(ioutils): add various helpers

--- include/tarp/ioutils.hxx
+++ include/tarp/ioutils.hxx
@@ -50,6 +50,37 @@ void println(Args &&...args) {
 // is not followed.
 std::pair<bool, std::string> touch(const std::string &fpath);
 
+// make fd point to /dev/null/ rather than its current file description.
+std::pair<bool, std::string> attach_fd_to_dev_null(int fd);
+
+// Close fd_to_redirect and reopen to point to the same file description as
+// pointed to by the target_description fd.
+std::pair<bool, std::string> duplicate_fd(int target_description,
+                                          int fd_to_redirect);
+
+// Check if fd is open.
+bool is_valid_fd(int fd);
+
+// Check if fd is open for readin or writing. Note a socket
+// is readable AND writable so both of these will return true.
+// Conversely, a pipe has readable and writable ends, so only
+// one of these will return true depending on which end of the
+// pipe they are called on.
+std::pair<bool, std::string> fd_open_for_reading(int fd);
+std::pair<bool, std::string> fd_open_for_writing(int fd);
+
+// check if two files are identical (same size, same content).
+// In case of error, the second result will be a non-empty string and the bool
+// should be ignored. The bool (if there is no error) indicates whether the
+// files are identical or not.
+std::pair<bool, std::string> files_identical(const std::string &fpath_a,
+                                             const std::string &fpath_b);
+
+// Generate a file with num_bytes bytes, with each byte
+// picked at random from the [0, 255] range.
+bool generate_file_random_bytes(const std::string &abspath,
+                                std::size_t num_bytes);
+
 }  // namespace io
 }  // namespace utils
 }  // namespace tarp
--- src/misc/ioutils.cxx
+++ src/misc/ioutils.cxx
@@ -1,10 +1,16 @@
 #include <tarp/ioutils.hxx>
 #include <tarp/string_utils.hxx>
 
+#include <fstream>
+#include <random>
+#include <filesystem>
+
 #include <cerrno>
+#include <cstring>
 
 extern "C" {
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 }
@@ -12,6 +18,173 @@ extern "C" {
 namespace tarp {
 namespace utils {
 namespace io {
+
+namespace fs = std::filesystem;
+using namespace std::string_literals;
+
+bool is_valid_fd( int fd )
+{
+	return fcntl( fd, F_GETFD ) != -1 || errno != EBADF;
+}
+
+std::pair< bool, std::string > fd_open_for_reading( int fd )
+{
+	errno = 0;
+
+	int res = fcntl( fd, F_GETFL );
+	if( res < 0 )
+	{
+		return { false, strerror( errno ) };
+	}
+
+	int file_access_mode = res & O_ACCMODE;
+
+	// NOTE: the access mode flags are not actually flags so they cannot
+	// be tested as 'file_access_mode & O_RDONLY'! for example,
+	// O_RDONLY on this system is actually 0 so '&' would always evaluate
+	// to 0 in that case.
+	if( file_access_mode == O_RDONLY )
+	{
+		return { true, "" };
+	}
+
+	if( file_access_mode == O_RDWR )
+	{
+		return { true, "" };
+	}
+
+	return { false, "" };
+}
+
+std::pair< bool, std::string > fd_open_for_writing( int fd )
+{
+	errno = 0;
+	int res = fcntl( fd, F_GETFL );
+	if( res < 0 )
+	{
+		return { false, strerror( errno ) };
+	}
+
+	auto file_access_mode = res & O_ACCMODE;
+
+	if( file_access_mode == O_WRONLY )
+	{
+		return { true, "" };
+	}
+
+	if( file_access_mode == O_RDWR )
+	{
+		return { true, "" };
+	}
+
+	return { false, "" };
+}
+
+std::pair< bool, std::string > duplicate_fd( int target_description, int fd_to_redirect )
+{
+	if( dup2( target_description, fd_to_redirect ) < 0 )
+	{
+		return { false, strerror( errno ) };
+	}
+	return { true, "" };
+}
+
+// point fd to /dev/null
+std::pair< bool, std::string > attach_fd_to_dev_null( int fd )
+{
+	int nulldev_fd = open( "/dev/null", O_RDWR );
+	if( nulldev_fd == -1 )
+	{
+		return { false, "Failed to open /dev/null: "s + strerror( errno ) };
+	}
+
+	auto [ ok, e ] = duplicate_fd( nulldev_fd, fd );
+	::close( nulldev_fd );
+	if( !ok )
+	{
+		return { false, "Failed /dev/null binding: "s + e };
+	}
+
+	return { true, "" };
+}
+
+std::pair< bool, std::string > files_identical( const std::string& fpath_a, const std::string& fpath_b )
+{
+	std::ifstream a, b;
+	a.open( fpath_a, std::ios_base::in | std::ios_base::binary );
+	if( !a.is_open() )
+	{
+		return { false, "failed to open " + fpath_a };
+	}
+
+	b.open( fpath_a, std::ios_base::in | std::ios_base::binary );
+	if( !b.is_open() )
+	{
+		return { false, "failed to open " + fpath_b };
+	}
+
+	if( fs::file_size( fpath_a ) != fs::file_size( fpath_b ) )
+	{
+		return { false, "" };
+	}
+
+	std::vector< char > buffer_a, buffer_b;
+	constexpr std::size_t BUFFSZ = 1024 * 1024;
+	buffer_a.resize( BUFFSZ );
+	buffer_b.resize( BUFFSZ );
+
+	std::size_t total_bytes_read_a = 0;
+	std::size_t total_bytes_read_b = 0;
+
+	while( !a.fail() and !b.fail() )
+	{
+		a.read( &buffer_a[ 0 ], buffer_a.size() );
+		auto bytes_read_a = a.gcount();
+		total_bytes_read_a += bytes_read_a;
+
+		b.read( &buffer_b[ 0 ], buffer_b.size() );
+		auto bytes_read_b = b.gcount();
+		total_bytes_read_b += bytes_read_b;
+
+		if( bytes_read_a != bytes_read_b or total_bytes_read_a != total_bytes_read_b )
+		{
+			throw std::logic_error( "Unexpected condition" );
+		}
+
+		for( unsigned i = 0; i < bytes_read_a; ++i )
+		{
+			if( buffer_a[ i ] != buffer_b[ i ] )
+			{
+				return { false, "" };
+			}
+		}
+	}
+	return { true, "" };
+}
+
+bool generate_file_random_bytes( const std::string& abspath, std::size_t num_bytes )
+{
+	std::ofstream of;
+	of.open( abspath, std::ios_base::out | std::ios_base::binary );
+	if( !of.is_open() )
+	{
+		return false;
+	}
+
+	std::random_device rd;
+	std::mt19937 rng( rd() );
+	std::uniform_int_distribution< std::mt19937::result_type > dist( 0, 255 );
+
+	for( unsigned i = 0; i < num_bytes; ++i )
+	{
+		std::uint8_t value = dist( rng );
+		of << value;
+	}
+
+	of.flush();
+	of.close();
+	return true;
+}
 
 std::pair<bool, std::string> touch(const std::string &fpath) {
     using namespace std::string_literals;
