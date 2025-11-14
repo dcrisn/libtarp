9b04dce4599a742820b9aca224dfc9b6e7965315
dcrisn <d.crisn@outlook.com>
2025-11-14 20:21:05 +0000

    chore: overhaul fletcher checksum code

--- include/tarp/hash/checksum/fletcher.hxx
+++ include/tarp/hash/checksum/fletcher.hxx
@@ -11,6 +11,7 @@
 namespace tarp {
 namespace hash {
 namespace checksum {
+using bits::byteswap;
 
 // Wikipedia [nov 2025, https://en.wikipedia.org/wiki/Fletcher%27s_checksum]:
 // > The Fletcher checksum cannot distinguish between blocks of all 0 bits
@@ -23,6 +24,7 @@ namespace fletcher {
 namespace impl {
 template<typename word, typename T>
 inline constexpr T find_max_num_additions_without_overflow();
+
 }  // namespace impl
 }  // namespace fletcher
 
@@ -48,6 +50,9 @@ struct fletcher_ctx {
     static inline constexpr accumulator_type MAX_ADD = fletcher::impl::
       find_max_num_additions_without_overflow<word_type, accumulator_type>();
 
+    static inline constexpr accumulator_type modulus =
+      std::numeric_limits<word>::max();
+
     // to check against MAX_ADD;
     // we perform a modulo when count == MAX_ADD,
     // in order to prevent overflow.
@@ -55,8 +60,10 @@ struct fletcher_ctx {
 
     // to allow processing of buffers that are not {word_type}-aligned;
     // see inet.hxx for more details (the same technique is applied).
+    // Here 'deficit' replaces 'truncated', since we can have more than
+    // 1 missing byte.
     std::uint8_t joint[sizeof(word_type)];
-    bool truncated = false;
+    std::uint8_t deficit = 0;
 
     acc sum1 = 0;
     acc sum2 = 0;
@@ -110,12 +117,6 @@ namespace fletcher {
 
 namespace impl {
 
-// get the modulus used for a fletcher checksum that reads
-// the input as a sequence of Ts. For example, for fletcher16
-// we use u16::max() = (2^16 - 1) as the modulus.
-// I.e. this is the maximum word value.
-template<typename T>
-static inline constexpr T modulus_v = std::numeric_limits<T>::max();
 
 // Find the maximum number of additions (inclusive)
 // that can be performed before overflow occurs.
@@ -177,335 +178,138 @@ inline constexpr T find_max_num_additions_without_overflow() {
 // Update the sums with the given value, while taking account of
 // the optimization discussed above. We perform a modulo when we've
 // reached the max_num_additions_without_modulo limit.
-template<typename context_t, typename T>
-static inline void update(context_t &ctx, T value, T modulus) {
+template<typename context_t>
+static inline void update(context_t &ctx, typename context_t::word value) {
     if (ctx.cnt == context_t::MAX_ADD) {
-        ctx.sum1 %= modulus;
-        ctx.sum2 %= modulus;
+        ctx.sum1 %= context_t::modulus;
+        ctx.sum2 %= context_t::modulus;
         ctx.cnt = 0;
     }
 
     ctx.sum1 = (ctx.sum1 + value);
     ctx.sum2 = (ctx.sum2 + ctx.sum1);
+
+    std::cerr << "=> adding value=" << static_cast<unsigned>(value)
+              << " sum1=" << ctx.sum1 << ", sum2=" << ctx.sum2 << std::endl;
     ctx.cnt++;
 }
 
 // Calculate and return the fletcherX checksum from the
 // 2 internal sums.
 template<typename context_t>
-typename context_t::word get_checksum(context_t &ctx) {
+typename context_t::checksum_t get_checksum(context_t &ctx) {
     using word = typename context_t::word;
-    using T = typename context_t::acc;
     using checksum_t = typename context_t::checksum_t;
 
-    ctx.sum1 %= modulus_v<T>;
-    ctx.sum2 %= modulus_v<T>;
-    ctx.run = 0;
+    ctx.sum1 %= context_t::modulus;
+    ctx.sum2 %= context_t::modulus;
+    ctx.cnt = 0;
 
-    checksum_t checksum = ctx.sum1 | (ctx.sum2 << tarp::bits::width_v<T>);
+    std::cerr << "reduction: ctx.sum1=" << ctx.sum1 << ", ctx.sum2=" << ctx.sum2
+              << " modulus=" << static_cast<std::uint64_t>(context_t::modulus)
+              << std::endl;
+
+    checksum_t checksum = ctx.sum1 | (ctx.sum2 << tarp::bits::width_v<word>);
     return checksum;
 }
 
 // Process a block of data and update the current checksum
 // value based on it. len need not be a multiple of sizeof(T).
 //
-// NOTE: nbo = network byte order.
-template<typename context_t, bool nbo>
-void process_chunks(context_t &ctx, const std::uint8_t *buff, std::size_t len) {
-    constexpr auto hbo = [](auto val) {
-        if constexpr (nbo) {
-            return bits::to_hbo(val);
-        }
-        return val;
-    };
+// If swap=true, then the bytes are swapped to reverse their endianness.
+template<bool swap, typename context_t>
+void update(context_t &ctx, const std::uint8_t *buff, std::size_t bufflen) {
+    using word_t = typename context_t::word;
 
-    if (ctx.truncated && bufflen > 0) {
-        // get the previous contribution so we
-        // can undo it (subtract it)
-        std::uint16_t prev = 0;
-        std::memcpy(&prev, ctx.joint, sizeof(prev));
+    if (ctx.deficit > 0 && bufflen > 0) {
+        word_t oldval = 0, newval = 0;
+        std::size_t l = sizeof(word_t) - ctx.deficit;
 
-        // next contribution
-        ctx.joint[1] = *buff;
-        std::uint16_t next = 0;
-        std::memcpy(&next, ctx.joint, sizeof(next));
+        // if still not enough bytes to form a word, then remain
+        // in deficit state, but consume these new bytes.
+        if (bufflen < ctx.deficit) {
+            std::memcpy(&oldval, ctx.joint, l);
+            std::memcpy(ctx.joint + l, buff, bufflen);
+            ctx.deficit -= bufflen;
+            std::memcpy(&newval, ctx.joint, sizeof(word_t) - ctx.deficit);
 
-        next -= prev;
-        ctx.sum += next;
-        bufflen--;
-        buff++;
-
-        // std::cerr << "adding [truncated] " << int_to_hexstring(next)
-        //           << ", (subtracted " << int_to_hexstring(prev)
-        //           << "), sum=" << int_to_hexstring(ctx.sum) << std::endl;
-        //
-        ctx.truncated = false;
-        std::memset(ctx.joint, 0, sizeof(std::uint16_t));
-    }
-
-
-
-    std::uint32_t n = len / sizeof(T);
-    std::uint32_t idx = 0;
-
-    while (n--) {
-        T currval = 0;
-        memcpy(&currval, msg + idx, sizeof(currval));
-
-        // NOTE: obviously, if typeof(J) == std:uint8_t, then endianness does
-        // not matter. Also, the widest type of checksum_t is std::uint64_t --
-        // and therefore the widest type of T is std::uint32_t as per the
-        // algorithm.
-        static_assert(!std::is_same_v<T, std::uint64_t>);
-        if constexpr (nbo) {
-            currval = bits::to_hbo(currval);
-        }
-
-        update(ctx,
-               currval,
-               modulus_v<decltype(currval)>,
-               max_num_additions_without_modulo);
-
-        idx += sizeof(currval);
-    }
-
-    return idx;
-}
-
-// The input is a buffer of bytes to be processed in sizeof(T)-byte chunks;
-// IOW the buffer is treated as a sequence of Ts.
-//
-// The buffer may be stored in network-byte order (big endian), in which
-// case that should be specified in the call to this function so that the
-// appropriate byte swapping can be performed as the data is read.
-//
-// If last_block=false, then the function stops when the number of
-// bytes remaining in the buffer is < sizeof(J). The index returned
-// will be < len.
-//
-// Otherwise if last_block=true, all of data is consumed and padding is
-// automatically added as required. In this case the index returned
-// will always be equal to len and can be ignored.
-//
-// Return:
-// - the index in data after the last byte that was processed; when all the
-// bytes have been processed (which is always the case when last_block=true)
-// this index is equal to len.
-template<typename T,
-         typename checksum_t,
-         typename sum_t,
-         bool network_byte_order,
-         std::uint32_t max_num_additions_without_modulo>
-std::uint32_t process_block(fletcher_ctx<sum_t> &ctx,
-                            const std::uint8_t *data,
-                            std::size_t len,
-                            bool last_block) {
-    std::uint32_t next_idx =
-      impl::process_chunks<T,
-                           checksum_t,
-                           sum_t,
-                           network_byte_order,
-                           max_num_additions_without_modulo>(ctx, data, len);
-
-    // pad and process this last chunk if this is the last block.
-    // If this is not the last block, stop short; do not process
-    // the remaining chunk of bytes if it is < sizeof(J) --
-    // otherwise we would break the final checksum.
-    if (last_block && next_idx < len) {
-        // number of unprocessed bytes
-        const auto n = len - next_idx;
-
-        // Here we've just copied a number of bytes < sizeof(J)
-        // to the smallest address of <value>.
-        // (1) If the host system has the same endianness as the data (e.g. both
-        // the host system and the data are big-endian or little endian) then
-        // there is nothing to do.
-        // (2) If the host system is big endian and the data is little endian,
-        // this is an unsupported situation. Data is expected to be converted
-        // to network byte order when moved between systems.
-        // (3) If the host system is little endian and the data is big endian,
-        // then we must shift up the bytes to increase their significance,
-        // so that the bits::to_hbo call in process_chunks() works as expected.
-        T value = 0;
-        std::memcpy(&value, data + next_idx, std::min(sizeof(value), n));
-        if constexpr (network_byte_order) {
-            const unsigned nshifts = (sizeof(value) - n) * 8;
-
-            // What we're doing here: if the host system is big-endian, then
-            // because we copied the bytes just now to the lowest address,
-            // shifting left will actually shift bits off the register,
-            // decreasing the value. OTOH if the system is little endian,
-            // shifting left will increase the value. IOW: we check whether
-            // we can increase the significance of the bytes we have, and
-            // if so, we do that.
-            if ((value << nshifts) > value) {
-                value <<= nshifts;
+            if constexpr (swap) {
+                oldval = byteswap(oldval);
+                newval = byteswap(newval);
             }
+
+            // subtract old contribution
+            update(ctx, -oldval);
+            // add new contribution
+            update(ctx, newval);
+            return;
         }
 
-        std::uint8_t buff[sizeof(T)];
-        std::memcpy(buff, &value, sizeof(value));
+        // else we have enough bytes to clear the deficit and form
+        // a full word
+        std::memcpy(&oldval, ctx.joint, l);
+        std::memcpy(ctx.joint+l, buff, ctx.deficit);
+        std::memcpy(&newval, ctx.joint, sizeof(word_t));
 
-        next_idx = impl::process_chunks<T,
-                                        checksum_t,
-                                        sum_t,
-                                        network_byte_order,
-                                        max_num_additions_without_modulo>(
-          ctx, buff, sizeof(T));
+        if constexpr (swap) {
+            oldval = byteswap(oldval);
+            newval = byteswap(newval);
+        }
+
+        update(ctx, -oldval);
+        update(ctx, newval);
+
+        buff += ctx.deficit;
+        bufflen -= ctx.deficit;
+        ctx.deficit = 0;
+        if (bufflen == 0) {
+            return;
+        }
     }
 
-    return next_idx;
-}
+    // process words
+    const std::size_t nwords = bufflen / sizeof(word_t);
+    for (std::size_t i = 0; i < nwords; ++i) {
+        word_t val;
+        std::memcpy(&val, buff, sizeof(val));
+        if constexpr (swap) val = byteswap(val);
+        update(ctx, val);
+        bufflen -= sizeof(word_t);
+        buff += sizeof(word_t);
+    }
 
-// Assume data is the one and only block that will be used for
-// computing the checksum.
-template<typename T,
-         typename checksum_t,
-         typename sum_t,
-         bool network_byte_order,
-         std::uint32_t max_num_additions_without_modulo>
-checksum_t process_all(const std::uint8_t *data, std::size_t len) {
-    fletcher_ctx<sum_t> ctx;
+    if (bufflen == 0) {
+        return;
+    }
 
-    process_block<T,
-                  checksum_t,
-                  sum_t,
-                  network_byte_order,
-                  max_num_additions_without_modulo>(ctx, data, len, true);
+    // trailing bytes (< sizeof(word)) ?
+    word_t val = 0;
+    std::memcpy(&val, buff, bufflen);
+    if constexpr (swap) val = byteswap(val);
+    update(ctx, val);
 
-    return get_checksum<T, checksum_t, sum_t>(ctx);
+    std::memcpy(ctx.joint, buff, bufflen);
+    ctx.deficit = sizeof(word_t) - bufflen;
 }
 
 }  // namespace impl
 
 namespace fletcher16 {
-namespace detail {
-// see https://en.wikipedia.org/wiki/Fletcher's_checksum.
-//   < sqrt( sum_t::max()*2 / T::max()),
-// with sum_t=u32 and T=u8.
-using T = std::uint8_t;
-using sum_t = std::uint32_t;
-using checksum_t = std::uint16_t;
-
-static inline constexpr std::uint32_t max_num_additions_without_modulo = 5802;
-}  // namespace detail
-
 inline std::uint16_t get_checksum(fletcher16_ctx &ctx) {
-    using namespace detail;
-    return impl::get_checksum<T, checksum_t, sum_t>(ctx);
-}
-
-template<bool big_endian>
-std::uint32_t process_block(fletcher16_ctx &ctx,
-                            const std::uint8_t *data,
-                            std::size_t len,
-                            bool last_block) {
-    using namespace detail;
-
-    //
-    return impl::process_block<T,
-                               checksum_t,
-                               sum_t,
-                               big_endian,
-                               max_num_additions_without_modulo>(
-      ctx, data, len, last_block);
-}
-
-template<bool big_endian>
-std::uint16_t process_all(const std::uint8_t *data, std::size_t len) {
-    using namespace detail;
-    return impl::process_all<T,
-                             checksum_t,
-                             sum_t,
-                             big_endian,
-                             max_num_additions_without_modulo>(data, len);
+    return impl::get_checksum(ctx);
 }
 }  // namespace fletcher16
 
-/*  Found by solving for c1 overflow: */
-/* n > 0 and n * (n+1) / 2 * (2^8-1) < (2^32-1). */
-
 namespace fletcher32 {
-namespace detail {
-// see https://en.wikipedia.org/wiki/Fletcher's_checksum.
-//   < sqrt( sum_t::max()*2 / T::max()),
-// with sum_t=u32 and T=u16.
-using T = std::uint16_t;
-using sum_t = std::uint32_t;
-using checksum_t = std::uint32_t;
-static inline constexpr std::uint32_t max_num_additions_without_modulo = 361;
-}  // namespace detail
 
 inline std::uint32_t get_checksum(fletcher32_ctx &ctx) {
-    using namespace detail;
-    return impl::get_checksum<T, checksum_t, sum_t>(ctx);
-}
-
-template<bool big_endian>
-std::uint32_t process_block(fletcher32_ctx &ctx,
-                            const std::uint8_t *data,
-                            std::size_t len,
-                            bool last_block) {
-    using namespace detail;
-
-    return impl::process_block<T,
-                               checksum_t,
-                               sum_t,
-                               big_endian,
-                               max_num_additions_without_modulo>(
-      ctx, data, len, last_block);
-}
-
-template<bool big_endian>
-std::uint32_t process_all(const std::uint8_t *data, std::size_t len) {
-    using namespace detail;
-
-    return impl::process_all<T,
-                             checksum_t,
-                             sum_t,
-                             big_endian,
-                             max_num_additions_without_modulo>(data, len);
+    return impl::get_checksum(ctx);
 }
 }  // namespace fletcher32
 
 namespace fletcher64 {
-namespace detail {
-// see https://en.wikipedia.org/wiki/Fletcher's_checksum.
-//   < sqrt( sum_t::max()*2 / T::max()),
-// with sum_t=u64 and T=u32.
-using T = std::uint32_t;
-using sum_t = std::uint64_t;
-using checksum_t = std::uint64_t;
-static inline constexpr std::uint32_t max_num_additions_without_modulo = 92680;
-}  // namespace detail
-
 inline std::uint64_t get_checksum(fletcher64_ctx &ctx) {
-    using namespace detail;
-    return impl::get_checksum<T, checksum_t, sum_t>(ctx);
-}
-
-template<bool big_endian>
-std::uint64_t process_all(const std::uint8_t *data, std::size_t len) {
-    using namespace detail;
-    return impl::process_all<T,
-                             checksum_t,
-                             sum_t,
-                             big_endian,
-                             max_num_additions_without_modulo>(data, len);
-}
-
-template<bool big_endian>
-std::uint32_t process_block(fletcher64_ctx &ctx,
-                            const std::uint8_t *data,
-                            std::size_t len,
-                            bool last_block) {
-    using namespace detail;
-    return impl::process_block<T,
-                               checksum_t,
-                               sum_t,
-                               big_endian,
-                               max_num_additions_without_modulo>(
-      ctx, data, len, last_block);
+    return impl::get_checksum(ctx);
 }
 }  // namespace fletcher64
 }  // namespace fletcher
@@ -521,19 +325,25 @@ std::uint32_t process_block(fletcher64_ctx &ctx,
 // network byte order and the implementation will internally
 // convert the bytes to host-byte-order as appropriate for
 // internal calculations.
-template<bool network_byte_order>
+template<bool endian_swap>
 std::uint16_t fletcher16(const std::uint8_t *data, std::size_t len) {
-    return fletcher::fletcher16::process_all<network_byte_order>(data, len);
+    fletcher16_ctx ctx;
+    fletcher::impl::update<endian_swap>(ctx, data, len);
+    return fletcher::fletcher16::get_checksum(ctx);
 }
 
-template<bool network_byte_order>
+template<bool endian_swap>
 std::uint32_t fletcher32(const std::uint8_t *data, std::size_t len) {
-    return fletcher::fletcher32::process_all<network_byte_order>(data, len);
+    fletcher32_ctx ctx;
+    fletcher::impl::update<endian_swap>(ctx, data, len);
+    return fletcher::fletcher32::get_checksum(ctx);
 }
 
-template<bool network_byte_order>
+template<bool endian_swap>
 std::uint64_t fletcher64(const std::uint8_t *data, std::size_t len) {
-    return fletcher::fletcher64::process_all<network_byte_order>(data, len);
+    fletcher64_ctx ctx;
+    fletcher::impl::update<endian_swap>(ctx, data, len);
+    return fletcher::fletcher64::get_checksum(ctx);
 }
 
 }  // namespace checksum
